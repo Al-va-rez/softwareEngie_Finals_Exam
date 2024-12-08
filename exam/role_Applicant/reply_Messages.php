8d29302c8a0513ad359cd1e85773f8852d3aaaba
Al-va-rez <167593484+Al-va-rez@users.noreply.github.com>
2024-12-08 17:09:57 +0800

    Add files via upload

---
+++ exam/role_Applicant/reply_Messages.php
@@ -0,0 +1,47 @@
+<?php
+    require_once 'core/handleForms.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Replying to Message</title>
+        <link rel="stylesheet" href="misc/myStyles.css">
+    </head>
+    <body class="hr">
+        <div class="position_Center">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>From:</td>
+                        <td><?= $_GET['recipient']?></td>
+                    </tr>
+                    <tr>
+                        <td>Message:</td>
+                        <td><?= $_GET['msg_Replying_To']?></td>
+                    </tr>
+                    <tr>
+                        <td>Reply:</td>
+                        <td>
+                            <form class="center_Form" action="core/handleForms.php?recipient=<?= $_GET['recipient']?>&msg_Replying_To=<?= $_GET['msg_Replying_To']?>" method="POST">
+
+                                <p><textarea id="inp_Msg" name="message"></textarea></p>
+
+                                <!-- SUBMIT -->
+                                <button type="submit" value="reply" name="btn_Send_Message">Send Reply</button>
+                            </form>
+                        </td>
+                    </tr>
+                    <tr>
+                        <!-- HOME -->
+                        <i><a href="dashboard.php">Back to Home</a></i>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <?php include '../response.php'; ?>
+    </body>
+</html>
---
+++ exam/role_HR/view_Applications.php
@@ -0,0 +1,78 @@
+<?php
+    require_once 'core/handleForms.php';
+
+    if (!isset($_SESSION['username']) && !isset($_SESSION['role'])) {
+        header('Location: ../login.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Job applications</title>
+        <link rel="stylesheet" href="../misc/myStyles.css">
+    </head>
+    <body class="hr">
+
+        <!-- ADMIN NAVBAR -->
+        <?php
+            include 'navbar.php';
+            include '../response.php';
+        ?>
+
+
+        <!-- SEARCH BOX -->
+        <form class="below_Navbar" action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="GET">
+            <input type="text" name="inp_Search" placeholder="search jobs here...">
+            <button type="submit" name="btn_Search">Search</button>
+            <a class="link_Btn" href="index.php">Clear Search Query</a>
+        </form>
+
+
+        <!-- JOB POSTS -->
+        <table>
+            <thead>
+                <tr>
+                    <th>Applications for <?php $_GET['job_Title'] ?></th>
+                </tr>
+                <tr>
+                    <th>Applicant</th>
+                    <th>Application Status</th>
+                    <th>Resume</th>
+                    <th>date_Applied</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- DISPLAY ALL -->
+                <?php if (!isset($_GET['btn_Search'])) { ?>
+                    <?php $HR_getAll_Applications = HR_getAll_Applications($pdo, $_GET['job_Title']); ?>
+                    <?php foreach ($HR_getAll_Applications as $application) { ?>
+                        <tr>
+                            <td><?= $application['applicant'] ?></td>
+                            <td><?= $application['status'] ?></td>
+                            <td><a href="../uploads/<?= htmlspecialchars($application['resume']) ?>" download>Download Resume</a></td>
+                            <td><?= $application['date_Applied'] ?></td>
+                            <td><a class="link_Btn" href="respond_Applications.php?applicant=<?= $application['applicant']?>&job_Title=<?= $_GET['job_Title']?>">Respond</a></td>
+                        </tr>
+                    <?php } ?>
+                    
+                <!-- SEARCH -->
+                <?php } else { ?>
+                    <?php $HR_search_Applications = HR_search_Applications($pdo, $_GET['inp_Search'], $_GET['job_Title']); ?>
+                    <?php foreach ($HR_search_Applications as $application) { ?>
+                        <tr>
+                            <td><?= $application['applicant'] ?></td>
+                            <td><?= $application['status'] ?></td>
+                            <td><a href="../uploads/<?= htmlspecialchars($application['resume']) ?>" download>Download Resume</a></td>
+                            <td><?= $application['date_Applied'] ?></td>
+                            <td><a class="link_Btn" href="respond_Applications.php?applicant=<?= $application['applicant']?>&job_Title=<?= $_GET['job_Title']?>">Respond</a></td>
+                        </tr>
+                    <?php } ?>
+                <?php } ?>
+            </tbody>
+        </table>
+    </body>
+</html>
