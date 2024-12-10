a0d0f2c272477c784263769022be5a77f04b16cf
Al-va-rez <167593484+Al-va-rez@users.noreply.github.com>
2024-12-10 19:13:15 +0800

    Add files via upload

---
+++ exam/response.php
@@ -0,0 +1,18 @@
+<!-- OPERATIONS MESSAGE: green text if it worked; otherwise red -->
+<?php if (isset($_SESSION['message']) && isset($_SESSION['status'])) { ?>
+    <div class="main">
+        <?php
+            if ($_SESSION['status'] == "200") {
+
+                echo "<h2 class='center_Form' style='color: green;'>{$_SESSION['message']}</h2>";
+
+            } else {
+                echo "<h2 class='center_Form' style='color: red;'>{$_SESSION['message']}</h2>";
+            }
+            
+        ?>
+    </div>
+<?php } 
+    unset($_SESSION['message']);
+    unset($_SESSION['status']);
+?>
